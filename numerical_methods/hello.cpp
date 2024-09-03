5c9f049f0d77cc6e076d252a87c9c368655c1a5c
GuryevVladislav <guryev.vs@yandex.ru>
2024-09-03 11:11:00 +0300

    Update hello.cpp
    Дополнение тестовых методов для ввода и вывода

--- numerical_methods/hello.cpp
+++ numerical_methods/hello.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
-
-using namespace std;
+#include <string>  // Для работы со строками
 
 int main() {
-   cout << "Привет! hot" << endl;
-   return 0;
+    // Ввод строки
+    std::string userString;
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, userString);  // Читаем строку, включая пробелы
+
+    // Ввод числа
+    int userNumber;
+    std::cout << "Введите целое число: ";
+    std::cin >> userNumber;
+
+    // Ввод массива
+    const int arraySize = 5;  // Размер массива
+    int userArray[arraySize];
+
+    std::cout << "Введите " << arraySize << " целых чисел:\n";
+    for (int i = 0; i < arraySize; ++i) {
+        std::cout << "Число " << (i + 1) << ": ";
+        std::cin >> userArray[i];
+    }
+
+    // Вывод введенных данных
+    std::cout << "\nВы ввели:\n";
+    std::cout << "Строка: " << userString << "\n";
+    std::cout << "Число: " << userNumber << "\n";
+    std::cout << "Массив: ";
+    for (int i = 0; i < arraySize; ++i) {
+        std::cout << userArray[i] << (i < arraySize - 1 ? ", " : "");
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
