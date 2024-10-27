590d4d0db5aae5311d743735b9dcb7a90219c19a
gurev vladislav <gurev@satel.org>
2024-10-28 01:18:59 +0300

    refactoring

--- numerical_methods/lab1_task1.cpp
+++ numerical_methods/lab1_task1.cpp
@@ -29,42 +29,16 @@ int main() {
 
     for (int i = 1; i < x.size(); i++) {
         x[i] = x[i - 1] + h;
-        outputFile << x[i] << " ";
         h=h+0.1;
         k[i][1] = calculate_f(x[i] + (h/2), v[i-1]);
-        outputFile << k[i][1] << " ";
-        k[i][2] = calculate_f(x[i] + (h/2), v[i-1] + (h/2) * k[i][1]);
-        outputFile << k[i][2] << " ";
-        k[i][3] = calculate_f(x[i] + h, v[i-1] + (h/2) * k[i][2]);
-        outputFile << k[i][3] << " ";
-        k[i][4] = calculate_f(x[i], v[i-1] + h * k[i][3]);
-        outputFile <<k[i][4] << " ";
+        k[i][2] = calculate_f(x[i] + (h/2), v[i-1] + (h/2) * k[i][1]);      
+        k[i][3] = calculate_f(x[i] + h, v[i-1] + (h/2) * k[i][2]);     
+        k[i][4] = calculate_f(x[i], v[i-1] + h * k[i][3]);        
         v[i] = v[i-1] + (h / 6) * (k[i][1] + 2 * k[i][2] + 2 * k[i][3] + k[i][4]);
-        outputFile << v[i] << endl;
+        
+        outputFile << x[i] << " " << k[i][1] << " " << k[i][2] << " " << k[i][3] << " " << k[i][4] << " " << v[i] << endl;
     }
 
-    // Вывод вектора x
-    cout << "Вектор x: ";
-    for (int i = 1; i < x.size(); ++i) {
-        cout << x[i] << " ";
-    }
-    cout << endl;
-
-    // Вывод вектора v
-    cout << "Вектор v: ";
-    for (int i = 1; i < v.size(); ++i) {
-        cout << v[i] << " ";
-    }
-    cout << endl;
-
-    // Вывод матрицы k
-    cout << "Матрица k: " << endl;
-    for (int i = 1; i < k.size(); ++i) {
-        for (int j = 1; j < k[i].size(); ++j) {
-            cout << k[i][j] << " ";
-        }
-        cout << endl;
-    }
 
     outputFile.close();
     cout << "Данные записаны в файл results.txt" << endl;
