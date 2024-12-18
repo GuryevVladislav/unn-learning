f39d16712744eb3ab561ad95d7896287857e199b
gurev vladislav <gurev@satel.org>
2024-12-18 20:19:28 +0300

    int task() {

---
+++ numerical_methods/sem5_lab2/task/main.cpp
@@ -0,0 +1,39 @@
+#include "mainTask.h"
+#include "testTask.h"
+#include "dataTask.h"
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string path="D:\\unn-learning\\numerical_methods\\sem5_lab2\\results\\";
+    filesystem::remove(path+"results.txt");
+    filesystem::remove(path+"input.txt");
+    cout<< "Enter n"<<endl;
+    int n;
+    cin>>n;
+    cout<<"Enter name task"<< endl;
+    string name;
+    cin>>name;
+    if (name == "test") {
+        TestTask test_task (n);
+        TaskData data(n);
+        data = test_task.calculate();
+        ofstream inputFile(path+"input.txt");
+        inputFile<<name<<" "<<n<<endl;
+        ofstream results(path+"results.txt");
+        for (int j = 1; j < n; j++) {
+            results<<data.i[j]<<" "<<data.x[j]<<" "<<data.v[j]<<" "<<data.u[j]<<" "<<data.diff[j]<<endl;
+        }
+    }
+    else {
+
+    }
+
+
+
+
+
+    return 0;
+  }
